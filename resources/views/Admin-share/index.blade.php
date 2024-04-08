2f26f911518c7473779b24364a3ecf0f327d49be
Hank <hank7891@gmail.com>
2024-04-08 19:25:45 +0800

    fix: 修正 js/css 載入方式
    改直接使用 php 輸出 正確基底網址

--- resources/views/Admin-share/index.blade.php
+++ resources/views/Admin-share/index.blade.php
@@ -8,27 +8,27 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href={{'admin-layout/plugins/fontawesome-free/css/all.min.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/plugins/fontawesome-free/css/all.min.css") ?>'>
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href={{'admin-layout/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css") ?>'>
     <!-- iCheck -->
-    <link rel="stylesheet" href={{'admin-layout/plugins/icheck-bootstrap/icheck-bootstrap.min.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/plugins/icheck-bootstrap/icheck-bootstrap.min.css") ?>'>
     <!-- JQVMap -->
-    <link rel="stylesheet" href={{'admin-layout/plugins/jqvmap/jqvmap.min.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/plugins/jqvmap/jqvmap.min.css") ?>'>
     <!-- Theme style -->
-    <link rel="stylesheet" href={{'admin-layout/dist/css/adminlte.min.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/dist/css/adminlte.min.css") ?>'>
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href={{'admin-layout/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/plugins/overlayScrollbars/css/OverlayScrollbars.min.css") ?>'>
     <!-- Daterange picker -->
-    <link rel="stylesheet" href={{'admin-layout/plugins/daterangepicker/daterangepicker.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/plugins/daterangepicker/daterangepicker.css") ?>'>
     <!-- summernote -->
-    <link rel="stylesheet" href={{'admin-layout/plugins/summernote/summernote-bs4.min.css'}}>
+    <link rel="stylesheet" href='<?= asset("admin-layout/plugins/summernote/summernote-bs4.min.css") ?>'>
     <!-- jQuery -->
-    <script src={{'admin-layout/plugins/jquery/jquery.min.js'}}></script>
+    <script src='<?= asset("admin-layout/plugins/jquery/jquery.min.js") ?>'></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src={{'admin-layout/plugins/jquery-ui/jquery-ui.min.js'}}></script>
+    <script src='<?= asset("admin-layout/plugins/jquery-ui/jquery-ui.min.js") ?>'></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 
     @include('Share/message-alert')
@@ -59,30 +59,30 @@
     $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src={{'admin-layout/plugins/bootstrap/js/bootstrap.bundle.min.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/bootstrap/js/bootstrap.bundle.min.js") ?>'></script>
 <!-- ChartJS -->
-<script src={{'admin-layout/plugins/chart.js/Chart.min.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/chart.js/Chart.min.js") ?>'></script>
 <!-- Sparkline -->
-<script src={{'admin-layout/plugins/sparklines/sparkline.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/sparklines/sparkline.js") ?>'></script>
 <!-- JQVMap -->
-<script src={{'admin-layout/plugins/jqvmap/jquery.vmap.min.js'}}></script>
-<script src={{'admin-layout/plugins/jqvmap/maps/jquery.vmap.usa.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/jqvmap/jquery.vmap.min.js") ?>'></script>
+<script src='<?= asset("admin-layout/plugins/jqvmap/maps/jquery.vmap.usa.js") ?>'></script>
 <!-- jQuery Knob Chart -->
-<script src={{'admin-layout/plugins/jquery-knob/jquery.knob.min.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/jquery-knob/jquery.knob.min.js") ?>'></script>
 <!-- daterangepicker -->
-<script src={{'admin-layout/plugins/moment/moment.min.js'}}></script>
-<script src={{'admin-layout/plugins/daterangepicker/daterangepicker.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/moment/moment.min.js") ?>'></script>
+<script src='<?= asset("admin-layout/plugins/daterangepicker/daterangepicker.js") ?>'></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src={{'admin-layout/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") ?>'></script>
 <!-- Summernote -->
-<script src={{'admin-layout/plugins/summernote/summernote-bs4.min.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/summernote/summernote-bs4.min.js") ?>'></script>
 <!-- overlayScrollbars -->
-<script src={{'admin-layout/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'}}></script>
+<script src='<?= asset("admin-layout/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") ?>'></script>
 <!-- AdminLTE App -->
-<script src={{'admin-layout/dist/js/adminlte.js'}}></script>
+<script src='<?= asset("admin-layout/dist/js/adminlte.js") ?>'></script>
 <!-- AdminLTE for demo purposes -->
-<script src={{'admin-layout/dist/js/demo.js'}}></script>
+<script src='<?= asset("admin-layout/dist/js/demo.js") ?>'></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src={{'admin-layout/dist/js/pages/dashboard.js'}}></script>
+<script src='<?= asset("admin-layout/dist/js/pages/dashboard.js") ?>'></script>
 </body>
 </html>
