6237cb94850e80fe5f89fd7933667f2d7d973015
Hank <hank7891@gmail.com>
2024-11-15 16:36:31 +0800

    feat: 添加 list 頁面路由設定

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::prefix('admin')->group(function () {
     Route::get('logout', [Controllers\Admin\IndexController::class, 'logout'])->middleware(Middleware\AdminIsLogin::class);
 
     Route::prefix('employee')->group(function () {
+        Route::get('list', [Controllers\Admin\EmployeeController::class, 'list'])->middleware(Middleware\AdminIsLogin::class);
         Route::get('edit', [Controllers\Admin\EmployeeController::class, 'edit'])->middleware(Middleware\AdminIsLogin::class);
         Route::post('edit', [Controllers\Admin\EmployeeController::class, 'editDo'])->middleware(Middleware\AdminIsLogin::class);
     });
